3a36a539ede566700737bd8be109a5a066ec2ed3
Chelley Sherman <chelleysherman@gmail.com>
2016-07-08 12:08:29 -0700

    commit

---
+++ HandTrackerPosition.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+
+public class HandTrackerPosition : MonoBehaviour{
+	Controller controller;
+	const float CoordinateScale = 1.0f / 200;
+	public GameObject handPrefab;
+	GameObject[] hand;
+	public int handIndex;
+
+	public int startingPitch = 1;
+	AudioSource audio;
+
+
+
+
+	void Start()
+	{
+		controller = new Controller();
+
+		audio = GetComponent<AudioSource>();
+		audio.pitch = startingPitch;
+
+
+
+
+	}
+
+	void Update(){
+		Frame frame = controller.Frame ();
+		if(frame.Hands.Count > 0){
+			Hand hand = frame.Hands[0];
+		}
+		Vector handCenter = frame.Hands[0].PalmPosition;
+		Debug.Log(handCenter);
+
+	
+
+		audio.pitch = startingPitch * (handCenter.y / 100f); 
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+}
